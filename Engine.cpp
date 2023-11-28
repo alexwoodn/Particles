8f9fcd05cd05d38863151c314b91da3957718c55
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-28 15:02:03 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -8,25 +8,12 @@ Engine::Engine()
     
 }
 // Run will call all the private functions
-Engine::void run()
+void Engine::run()
 {
     cout << "Starting Particle unit tests..." << endl;
     Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
     p.unitTests();
     cout << "Unit tests complete.  Starting engine..." << endl;
-  
-}
-
-
-Engine::RenderWindow m_Window
-{
-  	VideoMode desktop = VideoMode::getDesktopMode();
-    RenderWindow window(desktop, "Particle Viewer",
-}
-
-//vector for Particles
-vector<Particle> m_particles
-{
   
 }
 
