3455a0d6ee6063e4a8b16c95e8b83bbe1bbd8d95
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-28 14:43:24 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -7,14 +7,36 @@ Engine::Engine();
 
 // Run will call all the private functions
 Engine::void run();
+{
+    cout << "Starting Particle unit tests..." << endl;
+    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    p.unitTests();
+    cout << "Unit tests complete.  Starting engine..." << endl;
+  
+}
 
 
-Engine::RenderWindow m_Window;
+Engine::RenderWindow m_Window
+{
+  
+}
 
 //vector for Particles
-vector<Particle> m_particles;
+vector<Particle> m_particles
+{
+  
+}
 
 // Private functions for internal use only
-void Engine::input();
-void Engine::update(float dtAsSeconds);
-void Engine::draw();
+void Engine::input()
+{
+  
+}
+void Engine::update(float dtAsSeconds)
+{
+  
+}
+void Engine::draw()
+{
+  
+}
