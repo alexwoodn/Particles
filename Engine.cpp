244e3740954a45a0e74298fa8a5d8f09e1c4bf44
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-28 14:29:44 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,2 +1,19 @@
 #include "Engine.h"
 
+
+// The Engine constructor
+	Engine();
+
+	// Run will call all the private functions
+	void run();
+
+
+RenderWindow m_Window;
+
+//vector for Particles
+vector<Particle> m_particles;
+
+// Private functions for internal use only
+void input();
+void update(float dtAsSeconds);
+void draw();
