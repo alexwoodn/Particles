c7b000e5d2b62b31256ae02ec66b4f519973a448
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-28 15:20:07 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -5,31 +5,31 @@
 // The Engine constructor
 Engine::Engine()
 {
-    VideoMode desktop = VideoMode::getDesktopMode();
+	VideoMode desktop = VideoMode::getDesktopMode();
 	RenderWindow m_Window(desktop, "Particles", Style::Default);
 }
 // Run will call all the private functions
 void Engine::run()
 {
-    cout << "Starting Particle unit tests..." << endl;
-    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
-    p.unitTests();
-    cout << "Unit tests complete.  Starting engine..." << endl;
+	cout << "Starting Particle unit tests..." << endl;
+	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+	p.unitTests();
+	cout << "Unit tests complete.  Starting engine..." << endl;
   
 }
 
 // Private functions for internal use only
 void Engine::input()
 {
-  
+	
 }
 void Engine::update(float dtAsSeconds)
 {
-  
+	
 }
 void Engine::draw()
 {
-    window.clear()
-    window.draw(Particles);
+	window.clear()
+	window.draw(Particles);
     
 }
