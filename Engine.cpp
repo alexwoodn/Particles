d78b418a82fa86fe7b0f4ed394ea4b216d04c519
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-28 15:12:12 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -5,7 +5,8 @@
 // The Engine constructor
 Engine::Engine()
 {
-    RenderWindow m_Window(desktop, "Particles", Style::Default);
+    VideoMode desktop = VideoMode::getDesktopMode();
+	RenderWindow m_Window(desktop, "Particles", Style::Default);
 }
 // Run will call all the private functions
 void Engine::run()
