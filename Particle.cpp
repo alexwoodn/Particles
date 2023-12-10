fd00cc00b285dedee9c4d7cadc1041cd6c090dee
kevintheinventor <119896376+kevintheinventor@users.noreply.github.com>
2023-12-10 13:53:49 -0800

    Update Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -1,11 +1,9 @@
 #include "Particle.h"
 
-
 bool Particle::almostEqual(double a, double b, double eps)
 {
 	return fabs(a - b) < eps;
 }
-
 void Particle::unitTests()
 {
     int score = 0;
@@ -143,4 +141,90 @@ void Particle::unitTests()
     }
 
     cout << "Score: " << score << " / 7" << endl;
+}
+//mine
+Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) : m_A(2, numPoints), m_numPoints(numPoints), m_ttl(TTL)
+{
+    m_radiansPerSec = static_cast<float>(rand()) / RAND_MAX * M_PI;
+    m_cartesianPlane.setCenter(0, 0);
+    m_cartesianPlane.setSize(Vector2f(target.getSize().x, -1.0f * target.getSize().y));
+    m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
+    m_vx = rand() % 401 + 100;
+    m_vy = rand() % 401 + 100;
+    if (rand() % 2 != 0)
+    {
+        m_vx *= -1;
+    }
+    m_color1 = Color::White;
+    m_color2 = Color(rand() % 256, rand() % 256, rand() % 256);
+    //algorithm
+    float theta = static_cast<float>(rand()) / RAND_MAX * M_PI / 2;
+    float dTheta = 2 * M_PI / (m_numPoints - 1);
+
+    for (int j = 0; j < m_numPoints; j++)
+    {
+        float r = static_cast<float>(rand() % 61 + 20); // Random number between 20 and 80
+        float dx = r * cos(theta);
+        float dy = r * sin(theta);
+
+        m_A(0, j) = m_centerCoordinate.x + dx;
+        m_A(1, j) = m_centerCoordinate.y + dy;
+
+        theta += dTheta;
+    }
+}
+virtual void draw(RenderTarget& target, RenderStates states) const override
+{
+    VertexArray lines(TriangleFan, m_numPoints + 1);
+
+    Vector2f center;
+
+    center = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
+
+    lines[0].position = center;
+    lines[0].color = m_color1; 
+
+    for (int j = 1; j <= m_numPoints; ++j) {
+        Vector2f pixelPosition = target.mapCoordsToPixel(
+            Vector2f(m_A(0, j - 1), m_A(1, j - 1)),
+            m_cartesianPlane
+        );
+
+        lines[j].position = pixelPosition;
+        lines[j].color = m_color2; 
+    }
+
+    target.draw(lines, states);
+}
+void Particle::update(float dt) {
+    m_ttl -= dt;
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE); 
+    float dx = m_vx * dt;
+    float dy = m_vy * dt;
+    m_vy -= G * dt;
+    translate(dx, dy);
+}
+
+void Particle::translate(double xShift, double yShift) {
+    TranslationMatrix T(xShift, yShift);
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
+}
+
+void Particle::rotate(double theta) {
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R * m_A; 
+    translate(temp.x, temp.y);
+}
+
+void Particle::scale(double c) {
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A; 
+    translate(temp.x, temp.y);
 }
