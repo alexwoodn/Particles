5756d7bcf93f237e5902eb5424c7e5088a54bc99
kevintheinventor <119896376+kevintheinventor@users.noreply.github.com>
2023-12-10 14:54:08 -0800

    Update Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -185,7 +185,7 @@ void Particle :: draw(RenderTarget& target, RenderStates states) const
     lines[0].color = m_color1; 
 
     for (int j = 1; j <= m_numPoints; ++j) {
-        Vector2f pixelPosition = static_cast<Vector2f>(target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane);
+        Vector2f pixelPosition = static_cast<Vector2f>(target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane));
         lines[j].position = pixelPosition;
         lines[j].color = m_color2; 
     }
